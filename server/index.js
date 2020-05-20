88189955c90ffb75ea9f926573de6f57d696c31b
gajendra <gajendra@forktechnologies.com>
2020-05-20 21:11:53 +0530

    create routes, fix cors issue

--- server/index.js
+++ server/index.js
@@ -5,12 +5,14 @@ const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB Setup
 mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true });
 
 // App Setup
 app.use(morgan('combined')); // middleware to log request
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' })); // middleware to parse request
 router(app);
 
